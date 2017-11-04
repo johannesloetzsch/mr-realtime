0b799077b553509990b601bc8ff52346c7a0c294
pi <pi@localhost>
2017-11-04 05:22:11 +0100

    use inotifywait

--- facedetect/mjpg-streamer_facedetect.sh
+++ facedetect/mjpg-streamer_facedetect.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+raspistill --nopreview -w 800 -h 600 -o /tmp/stream/pic.jpg -tl 1000 -t 9999999 -th 0:0:0 &
+
 while true; do
-       	time raspistill --nopreview -w 800 -h 600 -o /tmp/stream/pic.jpg
-	time facedetect -d -o /tmp/stream/pic.facedetect.jpg /tmp/stream/pic.jpg
+	inotifywait /tmp/stream/pic.jpg; time facedetect -d -o /tmp/stream/pic.facedetect.jpg /tmp/stream/pic.jpg
 done
